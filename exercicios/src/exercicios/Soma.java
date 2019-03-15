b138f7e04dbc88dcaba0025b9d05cf4e3d8a12cf
paulohbraga <paulo@someletras.com.br>
2019-03-15 17:26:01 -0300

    Soma.java

---
+++ exercicios/src/exercicios/Soma.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class Soma {
+	public static void main(String [] args) {
+		int x, y, soma;
+		x = 10;
+		y = 20;
+		soma = x + y;
+		System.out.println(soma);
+	}
+
+}

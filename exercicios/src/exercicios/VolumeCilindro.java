b04460d7a3373462ec00b299bd8b04e71d9b5565
paulohbraga <paulo@someletras.com.br>
2019-03-18 00:01:16 -0300

    Calculo de volume do cubo

---
+++ exercicios/src/exercicios/VolumeCilindro.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+import exercicios.Volume;
+
+public class VolumeCilindro extends Volume {
+	
+	public static void main(String[] args) throws Exception {
+		Volume vol = new Volume();
+		double volume, raio, altura, pi;
+		pi = 3.14;
+
+		System.out.print("Insira o raio da base: ");
+		raio = vol.reader();
+		System.out.print("Insira a altura do cilindro: ");
+		altura = vol.reader();
+		volume = pi * Math.pow(raio , 2) * altura;
+		
+		System.out.println(volume);
+	}
+
+}

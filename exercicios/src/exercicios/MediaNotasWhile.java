600723747b7fff330a7b7541bb0db2dfec94bac7
paulohbraga <paulo@someletras.com.br>
2019-03-20 03:30:59 -0300

    Calculo de médias usando while

---
+++ exercicios/src/exercicios/MediaNotasWhile.java
@@ -0,0 +1,31 @@
+package exercicios;
+
+import exercicios.Reader;
+
+public class MediaNotasWhile {
+
+	public static void main(String[] args) throws Exception {
+		
+		//Reader leitor = new Reader();
+		double media = 0, totalnotas = 0; int numnotas;
+		
+		System.out.println("- - - - - Calcula Média notas - - - - -");
+		
+		System.out.print("Quantas notas deseja calcular? ");
+		numnotas = (int) Reader.leitor();
+		
+		int i = 0; // iterador
+		while (i < numnotas) {
+			System.out.print("Insira a nota: ");
+			totalnotas += Reader.leitor();	
+			System.out.println(totalnotas);
+			i++;
+
+		}
+		
+		media = totalnotas / numnotas;
+		
+		System.out.println("A média das notas é: " + String.format("%2f", media));
+	}
+
+}
